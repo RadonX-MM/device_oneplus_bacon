e6c7c1c8b77c3ba100b1659d5593fc5fe0752035
Joe Maples <frap129@gmail.com>
2015-09-20 20:52:32 -0400

    bacon: Add NO_OPTIMIZATIONS modules
    Signed-off-by: Joe Maples <frap129@gmail.com>

--- full_bacon.mk
+++ full_bacon.mk
@@ -12,6 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Disable Modules from being optimized
+export NO_OPTIMIZATIONS :=\
+  libqmiservices \
+  libsrec% \
+  %loc% \
+  %gps%
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
