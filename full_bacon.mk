665da52b2910582463902b2aa5cdfd89d0a40ef1
Joe Maples <frap129@gmail.com>
2015-09-21 17:57:45 -0400

    bacon: use GCC 4.9

--- full_bacon.mk
+++ full_bacon.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Use SaberMod GCC 4.9
+export TARGET_SM_AND := 4.9
+
 # Disable Modules from being optimized
 export NO_OPTIMIZATIONS :=\
   libqmiservices \
