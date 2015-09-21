d3daf637e12045248e49be3559b10e600eeaa67c
Joe Maples <frap129@gmail.com>
2015-09-21 16:44:10 -0400

    bacon: Dont Optimize camera_test
    Signed-off-by: Joe Maples <frap129@gmail.com>

--- full_bacon.mk
+++ full_bacon.mk
@@ -17,7 +17,8 @@ export NO_OPTIMIZATIONS :=\
   libqmiservices \
   libsrec% \
   %loc% \
-  %gps%
+  %gps% \
+  camera_test
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
