LineageOS/android_device_htc_msm8974-common

msm8974: add logging service
because logcats are hard.

This simply sends the logcat to sdcard so you can reboot into recovery and grab a log.

Change-Id: Ibed3a2d849a95391382d07b13684acfd49aa1218

rootdir/etc/loggy.sh
@@ -1,0 +1,5 @@
+#!/bin/sh
+# loggy.sh.
+
+date=`date +%F_%H-%M-%S`
+logcat -v time -f  /storage/sdcard0/logcat_${date}.txt

RadonX-MM/device_oneplus_bacon

Signed-off-by: Joe Maples <frap129@gmail.com>
Conflicts:
	msm8974-common.mk
	rootdir/Android.mk
	rootdir/etc/init.qcom.rc

rootdir/etc/loggy.sh
@@ -1,0 +1,5 @@
+#!/bin/sh
+# loggy.sh.
+
+date=`date +%F_%H-%M-%S`
+logcat -v time -f  /storage/sdcard0/cm121logcat_${date}.txt